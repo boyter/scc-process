4ef09901eb7989c3db98be3d535ef2a4c1921c98
Ben Boyter <ben@boyter.org>
2019-07-08 08:44:16 +1000

    PLaying around

---
+++ main.py
@@ -0,0 +1,85 @@
+import requests
+from multiprocessing import Pool
+import multiprocessing
+import hashlib
+import os
+import re
+import string
+import boto3
+
+def process(site):
+    pid = str(multiprocessing.current_process().pid)
+    filename, _ = process_path(site)
+    print('processing', site, pid, filename)
+    
+    os.chdir('/tmp')
+    os.system('rm -rf /tmp/scc-tmp-path-' + pid)
+    os.system('git clone --depth=1 ' + site + ' scc-tmp-path-' + pid)
+    os.system('scc -f json -o /tmp/' + filename + ' scc-tmp-path-' + pid)
+    
+
+    s3 = boto3.client('s3')
+    with open('/tmp/' + filename, 'rb') as f:
+        s3.upload_fileobj(f, 'sloccloccode', filename)
+
+    print('cleaning up', site, pid, filename)
+    os.system('rm /tmp/' + filename)
+    os.system('rm -rf /tmp/scc-tmp-path-' + pid)
+
+
+def process_path(path):
+    path = re.sub('', '', path, flags=re.MULTILINE )
+
+    s = [clean_string(x) for x in path.lower().split('/') if x != '']
+
+    if len(s) != 4:
+        return None, None
+
+    # Cheap clean check
+    for x in s:
+        if x == '':
+            return None, None
+
+    # File for json
+    filename = s[1].replace('.com', '').replace('.org', '')
+    filename += '.' + s[2]
+    filename += '.' + s[3].replace('.git', '') + '.json'
+
+    # Need path
+    path = s[3].replace('.git', '')
+
+    return (filename, path)
+
+
+def clean_string(s):
+    valid = string.ascii_lowercase
+    valid += string.digits
+    valid += '-'
+    valid += '.'
+    valid += '_'
+
+    clean = ''
+
+    for c in s:
+        if c in valid:
+            clean += c
+
+    return clean
+
+if __name__ == '__main__':
+    sites = []
+    count = 0
+    for line in open('urls.txt'):
+        count += 1
+        line = line.strip()
+
+        if line != '':
+            line += '.git'
+
+            # we already processed the first 2 million or so
+            if count > 2000000:
+                sites.append(line)
+
+    p = Pool(processes=32)
+    p.map(process, sites)
+    sites = []
